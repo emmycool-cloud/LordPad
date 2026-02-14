e1fcd30753c6d68dc6cab517168593d9e856e65c
emmycool-cloud <lordemmynence@gmail.com>
2026-02-14 09:11:12 +0100

    created the index,style and script js for my note pad app

---
+++ ReadME.txt
@@ -0,0 +1,23 @@
+ Lord Pad
+Lord Pad  is a clean, simple note-taking app designed to stay out of your way so you can focus on writing.
+---
+Why Use Lord Pad:
+
+* No Messy Duplicates: You can change a note's title 100 times, and it won't create 100 copies. It just updates the same note.
+
+* Fits Any Screen: It looks great on your laptop and automatically adjusts for your phone so you can write anywhere.
+
+*Privacy First: Your notes are saved in your browser's.
+
+*localStorage. This means your data never leaves your device.
+---
+ How to use it:
+1.  New Note: Click creat Note to clear the screen and start fresh.
+2.  Save: Enter a title and your text, then hit "Save."
+3.  Edit: Click any note in the sidebar to open it. Changes you save will overwrite the old version—no clones!
+4.  Delete: Click Delete to remove a note forever (don't worry, it asks for confirmation first).
+---
+ Built With
+* Vanilla JavaScript (ID-based storage logic)
+* CSS3 (Flexbox & Fluid Typography)
+* HTML5.
---
+++ script.js
@@ -0,0 +1,130 @@
+const noteTitle = document.getElementById("noteTitle");
+const noteInput = document.getElementById("noteInput");
+const noteList = document.getElementById("noteList");
+const addButton = document.getElementById("createNewNote");
+const saveButton = document.getElementById("saveNote");
+const deleteButton = document.getElementById("deleteNote");
+let noteBeingEdited = null;
+console.log(`start`);
+
+const loadNotes = () => {
+    const listContainer = document.getElementById("noteList");
+    listContainer.innerHTML = "";
+    let tempArray = [];
+
+    for(let i=0;i<localStorage.length;i++ ){
+        const getKey =localStorage.key(i);
+        if(getKey.startsWith("note_")){
+            const data = JSON.parse(localStorage.getItem(getKey));
+            tempArray.push(data);
+        }
+
+    }
+    tempArray.sort((a,b) => (b.time || 0) - (a.time || 0))
+
+    tempArray.forEach(note =>{
+        const div = document.createElement("button");
+        div.className = "note-item";
+        if(note.time === noteBeingEdited) {
+            div.classList.add("active")
+        }
+        div.innerText = note.title
+
+        div.onclick = () => {
+            document.getElementById("noteTitle").value = note.title;
+            document.getElementById("noteInput").value = note.body;
+            noteBeingEdited = note.time
+            loadNotes()
+        }
+
+        listContainer.appendChild(div)
+    })
+}
+
+
+const saveNote = () =>{
+    const noteTitle = document.getElementById("noteTitle").value;
+    const noteBody = document.getElementById("noteInput").value;
+   let updatedNoteTitle = noteTitle.trim()
+  for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        if (key.startsWith("note_")) {
+            const existingNote = JSON.parse(localStorage.getItem(key));
+            // If the title matches AND it's not the note we are currently editing
+            if (existingNote.title.toLowerCase() === noteTitle.toLowerCase() &&
+                existingNote.time !== noteBeingEdited) {
+                    console.log(`A note with this title already exists!`)
+                alert("A note with this title already exists!");
+                   return;
+                
+        }
+    }
+}
+     if(!noteTitle){
+         alert(`you can not save an Untitled note`);return
+     }
+     else if(!noteTitle && !noteBody){
+         alert(`you cannot save an empty note`);return
+     }
+     if(!noteBeingEdited){
+        noteBeingEdited = Date.now();
+     }
+    const noteContent = {
+        title:noteTitle,
+        body:noteBody,
+        time: noteBeingEdited
+    };
+    const storageKey = `note_${noteBeingEdited}`;
+    localStorage.setItem(storageKey, JSON.stringify(noteContent))
+alert(`${updatedNoteTitle} is saved`);
+loadNotes();
+}
+
+
+const addNote = () =>{
+     alert(`Creating a new note! Type in your title and content, then hit save`);
+noteBeingEdited = null;
+noteTitle.value = "";
+noteInput.value =""; 
+noteTitle.focus()  
+console.log(`Note ready`)
+   loadNotes();
+
+}
+
+
+
+const deleteNote = () =>{
+    const noteTitle= document.getElementById("noteTitle").value.trim();
+    
+    if(!noteBeingEdited){
+        alert(`please selecte a note first`);return;
+    }
+    const storageKey =`note_${noteBeingEdited}`;
+
+    if(localStorage.getItem(storageKey) === null){
+        alert(`note not found in storage`);return;
+    }
+
+    if(confirm(`Delete "${noteTitle}"?`)){
+        localStorage.removeItem(storageKey);
+         document.getElementById("noteTitle").value ="";
+        document.getElementById("noteInput").value = "";
+        noteBeingEdited = null
+        loadNotes()
+    }
+
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded',() =>{
+    
+    saveButton.addEventListener('click',saveNote)
+    deleteButton.addEventListener('click',deleteNote)
+    addButton.addEventListener('click',addNote)
+    loadNotes()
+})
+
+
